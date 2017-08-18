78640b308ae0dc45c99fd3c792293facb7b268d5
snoopa477 <al4721@nyu.edu>
2017-08-18 17:14:10 -0400

    added test 2

---
+++ test 2/test 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test 2
+//
+//  Created by Andy Lin on 2017/8/18.
+//  Copyright © 2017年 Andy Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
